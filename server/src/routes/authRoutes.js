b9003bd1aaf9f9a09b3a15c49b53c9f15fccb5f1
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2024-04-24 23:36:56 +0530

    Change the callback controller directly call

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,21 +1,7 @@
 import express from "express";
 const router = express.Router();
 import { login, signup } from "../controllers/authController.js";
-router.post("/login", async (req, res) => {
-	try {
-		await login(req, res);
-	} catch (error) {
-		console.error("Login error:", error);
-		res.status(500).json({ message: "Internal server error" });
-	}
-});
-router.post("/signup", async (req, res) => {
-	try {
-		await signup(req, res);
-	} catch (error) {
-		console.error("Login error:", error);
-		res.status(500).json({ message: "Internal server error" });
-	}
-});
+router.post("/login", login);
+router.post("/signup", signup);
 
 export default router;
