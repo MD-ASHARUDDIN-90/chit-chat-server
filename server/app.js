fdcaf88e5343abffc428e66f2b5738faa8bb8d1f
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2024-04-26 00:18:57 +0530

    🔨: multer config for image upload & cloudinary initialize

--- server/app.js
+++ server/app.js
@@ -4,13 +4,25 @@ import authRoutes from "./src/routes/authRoutes.js";
 import connectDB from "./src/db/db.js";
 import { config as dotenvConfig } from "dotenv";
 import { sendEmail } from "./src/utility/sendEmail.js";
+import { upload } from "./src/middleware/multerMiddleware.js";
+import { uploadToCloudinary } from "./src/utility/cloudinary.js";
+
 dotenvConfig();
+
 const app = express();
+
 const port = process.env.PORT || 8080;
+
+//middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//routes
 app.use("/api/auth", authRoutes);
 
+//temporary routes
 /*
  * Route to check if the mail is up and running.
  * Route to send an email.
@@ -35,11 +47,41 @@ app.post("/send-email", async (req, res) => {
 	}
 });
 
+// Route to upload a file to cloudinary.
+//
+// @param {Object} req - The request object.
+// @param {Object} req.file - The uploaded file.
+// @param {string} req.file.path - The path of the uploaded file.
+// @param {Object} res - The response object.
+// @return {string} The URL of the uploaded file.
+app.post("/upload", upload.single("avatar"), async (req, res) => {
+	console.log(req.file);
+	// Extract the path of the uploaded file.
+	const avatarLocalPath = req.file?.path;
+
+	// If the file is not found, throw an error.
+	if (!avatarLocalPath) {
+		throw new Error("File not found");
+	}
+	try {
+		// Upload the file to cloudinary and get the URL.
+		const url = await uploadToCloudinary(avatarLocalPath);
+		// Send the URL as the response.
+		res.status(200).send(url);
+	} catch (error) {
+		// Log the error and send a 500 status code.
+		console.error(error);
+		res.status(500).send("Error uploading file");
+	}
+});
+
+//error handling
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send("Something broke!");
 });
 
+//connect to database
 connectDB()
 	.then(() => {
 		app.listen(port, () => {
