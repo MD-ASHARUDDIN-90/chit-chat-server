fe69933bc1910a7652d45aa3f1f440238ae8442b
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2024-05-12 21:41:17 +0530

    Update login endpoint to return user data

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -64,10 +64,11 @@ async function login(req, res) {
 
 		await newRefreshToken.save();
 
+		const userData = user.toJSON();
+		delete userData.password;
+
 		res.status(200).json({
-			username: user.username,
-			email: user.email,
-			createdAt: user.createdAt,
+			...userData,
 			accessToken,
 			refreshToken,
 		});
