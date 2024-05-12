c20b629aa28a4fea8fac6f2340955a574fa866ab
MD-ASHARUDDIN-90 <asharansari90@gmail.com>
2024-05-12 19:57:57 +0530

    Fix multer production issue

--- src/middleware/multerMiddleware.js
+++ src/middleware/multerMiddleware.js
@@ -1,8 +1,10 @@
 import multer from "multer";
 
+const fileDirectory = process.env.NODE_ENV === "production" ? "/tmp" : "./tmp";
+
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, "./public/temp");
+		cb(null, fileDirectory);
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
