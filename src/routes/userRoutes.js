f49836e22feab08cb695910694d01f3e69f4d950
MD ASHARUDDIN <119596353+MD-ASHARUDDIN-90@users.noreply.github.com>
2024-05-26 02:26:17 +0530

    Develop (#24)
    * ✨: Feature of forget password & reset password
    
    * Feature/follow friend request (#23)
    
    * ✨: Extend user modal & add people you may know feature
    
    * ✨: Add feature follow, un-follow, remove suggestion, search

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,10 @@ import {
 	updateDisplayPicture,
 	updateUserData,
 	updateUserPassword,
+	getPeopleYouMayKnow,
+	followUserRequestHandler,
+	getPeopleYouFollow,
+	searchUsers,
 } from "../controllers/userController.js";
 import { uploadMiddleware } from "../middleware/multerMiddleware.js";
 
@@ -15,5 +19,9 @@ router.get("/posts", getMyPosts);
 router.put("/update", uploadMiddleware, updateUserData);
 router.put("/update/display-picture", uploadMiddleware, updateDisplayPicture);
 router.put("/update-password", updateUserPassword);
+router.get("/people-you-may-know", getPeopleYouMayKnow);
+router.post("/follow-user", followUserRequestHandler);
+router.get("/following", getPeopleYouFollow);
+router.get("/search-people", searchUsers);
 
 export default router;
